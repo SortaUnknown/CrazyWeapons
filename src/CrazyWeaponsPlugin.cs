bcf4b5d0425c357e36e267ba5cf5a11ba1a53ad9
SortaUnknown <roi.anubis@gmail.com>
2023-02-18 01:33:15 +0100

    Hotfix version bump

--- src/CrazyWeaponsPlugin.cs
+++ src/CrazyWeaponsPlugin.cs
@@ -8,7 +8,7 @@ using System.Security.Permissions;
 
 namespace CrazyWeaponsPlugin
 {
-    [BepInPlugin("HelloThere.CrazyWeapons", "Crazy Weapons", "1.2")]
+    [BepInPlugin("HelloThere.CrazyWeapons", "Crazy Weapons", "1.2.1")]
     public class CrazyWeaponsMod : BaseUnityPlugin
     {
         public void OnEnable()
